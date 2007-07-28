368baa25e3d7618880667c61492ed3454d0abdea
Nigel Cunningham <nigel@suspend2.net>
2007-07-28 20:34:12 +1000

    Fix another missed suspend2->toi conversion.
    Thanks again to Matthias Hensler.
    
    Signed-off-by: Nigel Cunningham <nigel@suspend2.net>

--- include/asm-powerpc/suspend.h
+++ include/asm-powerpc/suspend.h
@@ -6,7 +6,7 @@ static inline int arch_prepare_suspend(void) { return 0; }
 void save_processor_state(void);
 void restore_processor_state(void);
 
-#define suspend2_faulted (0)
-#define clear_suspend2_fault() do { } while(0)
+#define toi_faulted (0)
+#define clear_toi_fault() do { } while(0)
 
 #endif /* __ASM_POWERPC_SUSPEND_H */
