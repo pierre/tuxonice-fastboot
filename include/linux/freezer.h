72728c01936aeffd91cdb745efca04943f11a85d
Nigel Cunningham <nigel@tuxonice.net>
2008-11-12 08:28:48 +1100

    [TuxOnIce] Fix compilation with CGROUP_FREEZER and !PM_SLEEP
    Fix a compilation error when CGROUP_FREEZER is defined, but
    PM_SLEEP isn't.
    
    Signed-off-by: Nigel Cunningham <nigel@tuxonice.net>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -121,6 +121,7 @@ static inline void set_freezable(void)
 	current->flags &= ~PF_NOFREEZE;
 }
 
+#ifdef CONFIG_PM_SLEEP
 extern int freezer_state;
 #define FREEZER_OFF 0
 #define FREEZER_FILESYSTEMS_FROZEN 1
@@ -131,6 +132,9 @@ static inline int freezer_is_on(void)
 {
 	return freezer_state == FREEZER_FULLY_ON;
 }
+#else
+static inline int freezer_is_on(void) { return 0; }
+#endif
 
 extern void thaw_kernel_threads(void);
 
