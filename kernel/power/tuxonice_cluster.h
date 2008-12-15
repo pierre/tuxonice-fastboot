06c0fdd713e66c17c36e72e76a6d52383e2d64ae
Nigel Cunningham <nigel@tuxonice.net>
2008-12-15 21:34:36 +1100

    Cluster module support.
    Add code that applies on top of the cluster support, for building as
    modules.
    
    Signed-off-by: Nigel Cunningham <nigel@tuxonice.net>

--- kernel/power/tuxonice_cluster.h
+++ kernel/power/tuxonice_cluster.h
@@ -9,9 +9,11 @@
 
 #ifdef CONFIG_TOI_CLUSTER
 extern int toi_cluster_init(void);
+extern void toi_cluster_exit(void);
 extern void toi_initiate_cluster_hibernate(void);
 #else
 static inline int toi_cluster_init(void) { return 0; }
+static inline void toi_cluster_exit(void) { }
 static inline void toi_initiate_cluster_hibernate(void) { }
 #endif
 
