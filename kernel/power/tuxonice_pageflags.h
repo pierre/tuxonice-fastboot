2888e61ad1ca47a2ad2745e0b42fff6ab4d4833c
Nigel Cunningham <nigel@tuxonice.net>
2009-01-03 08:48:00 +1100

    Switch to using swsusp's bitmaps.
    Switch from using dynpageflags code to swsusp's bitmaps. This reduces
    the number of lines of code added and files touched by TuxOnIce, and may
    also help with a reported case where swsusp is working and TuxOnIce
    doesn't.
    
    Signed-off-by: Nigel Cunningham <nigel@tuxonice.net>

--- kernel/power/tuxonice_pageflags.h
+++ kernel/power/tuxonice_pageflags.h
@@ -4,60 +4,59 @@
  * Copyright (C) 2004-2008 Nigel Cunningham (nigel at tuxonice net)
  *
  * This file is released under the GPLv2.
- *
- * TuxOnIce needs a few pageflags while working that aren't otherwise
- * used. To save the struct page pageflags, we dynamically allocate
- * a bitmap and use that. These are the only non order-0 allocations
- * we do.
- *
- * NOTE!!!
- * We assume that PAGE_SIZE - sizeof(void *) is a multiple of
- * sizeof(unsigned long). Is this ever false?
  */
 
-#include <linux/dyn_pageflags.h>
-#include <linux/suspend.h>
+#ifndef KERNEL_POWER_TUXONICE_PAGEFLAGS_H
+#define KERNEL_POWER_TUXONICE_PAGEFLAGS_H
 
-extern struct dyn_pageflags pageset1_map;
-extern struct dyn_pageflags pageset1_copy_map;
-extern struct dyn_pageflags pageset2_map;
-extern struct dyn_pageflags page_resave_map;
-extern struct dyn_pageflags io_map;
-extern struct dyn_pageflags nosave_map;
-extern struct dyn_pageflags free_map;
+#include "power.h"
 
-#define PagePageset1(page) (test_dynpageflag(&pageset1_map, page))
-#define SetPagePageset1(page) (set_dynpageflag(&pageset1_map, page))
-#define ClearPagePageset1(page) (clear_dynpageflag(&pageset1_map, page))
+extern struct memory_bitmap pageset1_map;
+extern struct memory_bitmap pageset1_copy_map;
+extern struct memory_bitmap pageset2_map;
+extern struct memory_bitmap page_resave_map;
+extern struct memory_bitmap io_map;
+extern struct memory_bitmap nosave_map;
+extern struct memory_bitmap free_map;
 
-#define PagePageset1Copy(page) (test_dynpageflag(&pageset1_copy_map, page))
-#define SetPagePageset1Copy(page) (set_dynpageflag(&pageset1_copy_map, page))
+#define PagePageset1(page) (memory_bm_test_bit(&pageset1_map, page_to_pfn(page)))
+#define SetPagePageset1(page) (memory_bm_set_bit(&pageset1_map, page_to_pfn(page)))
+#define ClearPagePageset1(page) (memory_bm_clear_bit(&pageset1_map, page_to_pfn(page)))
+
+#define PagePageset1Copy(page) (memory_bm_test_bit(&pageset1_copy_map, page_to_pfn(page)))
+#define SetPagePageset1Copy(page) (memory_bm_set_bit(&pageset1_copy_map, page_to_pfn(page)))
 #define ClearPagePageset1Copy(page) \
-	(clear_dynpageflag(&pageset1_copy_map, page))
+	(memory_bm_clear_bit(&pageset1_copy_map, page_to_pfn(page)))
 
-#define PagePageset2(page) (test_dynpageflag(&pageset2_map, page))
-#define SetPagePageset2(page) (set_dynpageflag(&pageset2_map, page))
-#define ClearPagePageset2(page) (clear_dynpageflag(&pageset2_map, page))
+#define PagePageset2(page) (memory_bm_test_bit(&pageset2_map, page_to_pfn(page)))
+#define SetPagePageset2(page) (memory_bm_set_bit(&pageset2_map, page_to_pfn(page)))
+#define ClearPagePageset2(page) (memory_bm_clear_bit(&pageset2_map, page_to_pfn(page)))
 
-#define PageWasRW(page) (test_dynpageflag(&pageset2_map, page))
-#define SetPageWasRW(page) (set_dynpageflag(&pageset2_map, page))
-#define ClearPageWasRW(page) (clear_dynpageflag(&pageset2_map, page))
+#define PageWasRW(page) (memory_bm_test_bit(&pageset2_map, page_to_pfn(page)))
+#define SetPageWasRW(page) (memory_bm_set_bit(&pageset2_map, page_to_pfn(page)))
+#define ClearPageWasRW(page) (memory_bm_clear_bit(&pageset2_map, page_to_pfn(page)))
 
-#define PageResave(page) (page_resave_map.bitmap ? \
-	test_dynpageflag(&page_resave_map, page) : 0)
-#define SetPageResave(page) (set_dynpageflag(&page_resave_map, page))
-#define ClearPageResave(page) (clear_dynpageflag(&page_resave_map, page))
+#define PageResave(page) (page_resave_map.zone_bm_list ? \
+	memory_bm_test_bit(&page_resave_map, page_to_pfn(page)) : 0)
+#define SetPageResave(page) (memory_bm_set_bit(&page_resave_map, page_to_pfn(page)))
+#define ClearPageResave(page) (memory_bm_clear_bit(&page_resave_map, page_to_pfn(page)))
 
-#define PageNosave(page) (nosave_map.bitmap ? \
-		test_dynpageflag(&nosave_map, page) : 0)
-#define SetPageNosave(page) (set_dynpageflag(&nosave_map, page))
-#define ClearPageNosave(page) (clear_dynpageflag(&nosave_map, page))
+#define PageNosave(page) (nosave_map.zone_bm_list ? \
+		memory_bm_test_bit(&nosave_map, page_to_pfn(page)) : 0)
+#define SetPageNosave(page) (memory_bm_set_bit(&nosave_map, page_to_pfn(page)))
+#define ClearPageNosave(page) (memory_bm_clear_bit(&nosave_map, page_to_pfn(page)))
 
-#define PageNosaveFree(page) (free_map.bitmap ? \
-		test_dynpageflag(&free_map, page) : 0)
-#define SetPageNosaveFree(page) (set_dynpageflag(&free_map, page))
-#define ClearPageNosaveFree(page) (clear_dynpageflag(&free_map, page))
+#define PageNosaveFree(page) (free_map.zone_bm_list ? \
+		memory_bm_test_bit(&free_map, page_to_pfn(page)) : 0)
+#define SetPageNosaveFree(page) (memory_bm_set_bit(&free_map, page_to_pfn(page)))
+#define ClearPageNosaveFree(page) (memory_bm_clear_bit(&free_map, page_to_pfn(page)))
 
-extern void save_dyn_pageflags(struct dyn_pageflags *pagemap);
-extern int load_dyn_pageflags(struct dyn_pageflags *pagemap);
+extern void save_pageflags(struct memory_bitmap *pagemap);
+extern int load_pageflags(struct memory_bitmap *pagemap);
 extern int toi_pageflags_space_needed(void);
+
+#define BITMAP_FOR_EACH_SET(map, pfn) \
+	memory_bm_position_reset(&map); \
+	for (pfn = memory_bm_next_pfn(&map); pfn != BM_END_OF_MAP; \
+		pfn = memory_bm_next_pfn(&map))
+#endif
